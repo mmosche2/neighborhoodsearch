b70b37d1ecb8653fbc06b32f22396f62889f7184
Mark <mark.moschel@gmail.com>
2013-07-13 13:53:32 -0500

    add birth rate table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,99 @@
+require "httparty"
+
 class HomeController < ApplicationController
   def index
+  	@query = params["query"]
+  	if @query && COMMUNITY_AREAS.include?(@query.downcase)
+  		@birth_rates = HTTParty.get(BIRTH_RATE_API_URL,
+						:query => {:community_area_name => @query}
+			).parsed_response.first
+  	end
   end
 
-  def query
 
-  end
-  
+  private
+
+  	BIRTH_RATE_API_URL = "http://data.cityofchicago.org/resource/4arr-givg.json"
+
+
+
+		COMMUNITY_AREAS = ['rogers park',
+'west ridge',
+'uptown',
+'lincoln square',
+'north center',
+'lake view',
+'lincoln park',
+'near north side',
+'edison park',
+'norwood park',
+'jefferson park',
+'forest glen',
+'north park',
+'albany park',
+'portage park',
+'irving park',
+'dunning',
+'montclaire',
+'belmont cragin',
+'hermosa',
+'avondale',
+'logan square',
+'humboldt park',
+'west town',
+'austin',
+'west garfield park',
+'east garfield park',
+'near west side',
+'north lawndale',
+'south lawndale',
+'lower west side',
+'loop',
+'near south side',
+'armour square',
+'douglas',
+'oakland',
+'fuller park',
+'grand boulevard',
+'kenwood',
+'washington park',
+'hyde park',
+'woodlawn',
+'south shore',
+'chatham',
+'avalon park',
+'south chicago',
+'burnside',
+'calumet heights',
+'roseland',
+'pullman',
+'south deering',
+'east side',
+'west pullman',
+'riverdale',
+'hegewisch',
+'garfield ridge',
+'archer heights',
+'brighton park',
+'mckinley park',
+'bridgeport',
+'new city',
+'west elsdon',
+'gage park',
+'clearing',
+'west lawn',
+'chicago lawn',
+'west englewood',
+'englewood',
+'greater grand crossing',
+'ashburn',
+'auburn gresham',
+'beverly',
+'washington heights',
+'mount greenwood',
+'morgan park',
+'o\'hare',
+'edgewater',
+'chicago']
+		
 end
